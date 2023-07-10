4ab7905626ca8918547b1b0ec28cd1b47cd566c4
danil <dan.craw@yandex.ru>
2023-07-10 10:02:30 +0300

    add apis

---
+++ apis/README.md
@@ -0,0 +1,18 @@
+## FastAPI CRUD app with async DB and SQLAlchemy
+
+## Run app
+```bash
+docker-compose -f docker-compose.dev.yml up
+```
+## Run tests
+```bash
+docker-compose -f docker-compose.test.yml run web pytest -vv
+```
+### Technologies used
+FastAP
+Pydantic
+Swagger
+SQLAlchemy
+Typing
+PostgreSQL
+Pytest
---
+++ apis/app/main.py
@@ -0,0 +1,30 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+
+from apis.app.api.routes import api
+from core.app_events import start_app, stop_app
+from core.config import config
+
+logger = logging.getLogger("uvicorn.error")
+
+
+def get_application() -> FastAPI:
+    application = FastAPI(
+        title=config.SERVICE_NAME,
+        description=config.DESCRIPTION,
+        debug=config.DEBUG,
+    )
+    application.add_event_handler("startup", start_app)
+    application.add_event_handler("shutdown", stop_app)
+
+    application.include_router(api.psychologists_router, prefix=config.API_V1_STR)
+
+    return application
+
+
+app = get_application()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
